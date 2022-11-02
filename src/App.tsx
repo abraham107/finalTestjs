1d9cc30b26c0b3257c45baaed62d61915d37a5c2
abraham107 <abrahamsamuel107@gmail.com>
2022-11-02 19:09:34 -0400

    Added routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 // common components
 import Header from './views/components/header';
 import Footer from './views/components/footer';
+
+// content components
+import Home from './views/content/home';
 
 // styles and fonts
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,12 +19,18 @@ import 'jquery/dist/jquery.min.js';
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/services" element={<Home />}></Route>
+        <Route path="/projects" element={<Home />}></Route>
+        <Route path="/contact" element={<Home />}></Route>
 
-
-      <h1 className='container'>Hello, World!</h1>
-
+      </Routes>
       <Footer></Footer>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/views/content/home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home()
+{
+    return(
+        <div className="container">
+            <h1>Welcome to our site!</h1>
+            <hr/>
+        </div>
+    );
+}
+
+export default Home;
