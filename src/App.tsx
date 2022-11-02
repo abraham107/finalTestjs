2ab2e318d35b15cf1f67d7ec9036ebe7239691b4
abraham107 <abrahamsamuel107@gmail.com>
2022-11-02 18:47:30 -0400

    added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// common components
+import Header from './views/components/header';
 
 // styles and fonts
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
+
+// JavaScript libraries
+import 'jquery/dist/jquery.min.js';
+
 function App() {
   return (
     <div className="App">
+      <Header></Header>
+
+
       <h1 className='container'>Hello, World!</h1>
     </div>
   );
